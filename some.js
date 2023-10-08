127c00f62d556864e7e1fed969bc47aa8670247f
ulugbek_developer <ulugbekqodirov222002@gmail.com>
2023-10-08 22:50:31 +0500

    initiy commit

--- some.js
+++ some.js
@@ -1,8 +1,8 @@
 console.log('one');
 var ww = process.env.NODE_ENV.trim.toLowerCase();
-if (ww === 'production') {
+          if (ww === 'production') {
   console.log('Another one');
-} else {
+}        else {
   console.log('test');
 }
 
