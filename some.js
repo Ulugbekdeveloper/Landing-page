771428d59e9182414dbde3dc3ddab862948b29e4
ulugbek_developer <ulugbekqodirov222002@gmail.com>
2023-10-08 22:48:46 +0500

    efefef

---
+++ some.js
@@ -0,0 +1,12 @@
+console.log('one');
+var ww = process.env.NODE_ENV.trim.toLowerCase();
+if (ww === 'production') {
+  console.log('Another one');
+} else {
+  console.log('test');
+}
+
+
+
+
+let name  = "hrthrnt" ;let age = 32
